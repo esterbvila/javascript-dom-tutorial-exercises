b6d3316f8dffd58a0cabdba225733635b335d6aa
Ester Batllori I Vila <ester.batllori@gmail.com>
2021-03-26 10:27:13 +0000

    ex 6

--- exercises/05-Create-DOM-Element-First/index.js
+++ exercises/05-Create-DOM-Element-First/index.js
@@ -2,5 +2,7 @@ var elem = document.createElement("H1");
 elem.innerHTML = "Hello World";
 document.querySelector("#myDiv").appendChild(elem);
 
-//Your code here
-// Don'change the code above
+let elem2 = document.createElement("p");
+elem2.innerHTML = "Hello World";
+elem2.style.background = "yellow";
+document.body.appendChild(elem2);
--- exercises/06-Create-DOM-Element-Second/index.js
+++ exercises/06-Create-DOM-Element-Second/index.js
@@ -1,3 +1,3 @@
-//you code here
+document.body.innerHTML = "<IMG HREF="http://placehold.it/350x150"></IMG>";
 document.body.innerHTML = "<h1>Hello World</h1>";
 
